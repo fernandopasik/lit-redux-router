05ea544a1a475482da715a418a4bb4ecdde68699
Fernando Pasik <fernando@pasik.com.ar>
2018-10-13 18:31:42 +0100

    chore: setup jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.(js|ts)?(x)', '**/?(*.)+(spec|test).(js|ts)?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
+};
