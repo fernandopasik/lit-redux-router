7791ffd019e2eefb2fafd7108582c7213c790f4f
Fernando Pasik <fernando@pasik.com.ar>
2018-10-13 21:04:07 +0100

    feat: add route on first update

--- src/route.ts
+++ src/route.ts
@@ -1,13 +1,21 @@
-import { LitElement, html } from '@polymer/lit-element';
+import { LitElement, html, property } from '@polymer/lit-element';
 import { connect } from 'pwa-helpers/connect-mixin.js';
 
 import { Store } from 'redux';
 import { LazyStore } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
+import { addRoute } from './actions';
 import { State } from './reducer';
 
 export default (store: Store<State> & LazyStore) => {
   class Route extends connect(store)(LitElement) {
+    @property({ type: String })
+    path;
+
+    firstUpdated() {
+      store.dispatch(addRoute(this.path));
+    }
+
     render() {
       return html`<slot></slot>`;
     }
