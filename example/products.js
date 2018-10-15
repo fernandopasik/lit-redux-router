c3d007b6ad173d9522b05b5a79dd9c9f39f69cc1
Fernando Pasik <fernando@pasik.com.ar>
2018-10-15 23:07:02 +0100

    refactor: example component

--- example/products.js
+++ example/products.js
@@ -2,7 +2,15 @@ import { LitElement, html } from '@polymer/lit-element';
 
 export default class MyProduct extends LitElement {
   render() {
-    return html`<h1>Product</h1>`;
+    return html`
+      <style>
+        h1 {
+          margin-top: 0;
+          margin-bottom: 16px;
+        }
+      </style>
+      <h1>Product</h1>
+    `;
   }
 }
 
