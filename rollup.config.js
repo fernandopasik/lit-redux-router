1a2b562b4cf2afd960eb778a189b644703d43499
Fernando Pasik <fernando@pasik.com.ar>
2018-10-18 21:40:42 +0100

    chore: check for file size

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'dist/index.js',
+  output: {
+    file: 'bundle.js',
+    format: 'esm',
+  },
+  onwarn(warning) {
+    if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+      console.error(`(!) ${warning.message}`); // eslint-disable-line no-console
+    }
+  },
+  plugins: [
+    resolve({
+      only: ['regexparam'],
+    }),
+    terser({
+      warnings: true,
+      mangle: {
+        module: true,
+      },
+    }),
+  ],
+};
