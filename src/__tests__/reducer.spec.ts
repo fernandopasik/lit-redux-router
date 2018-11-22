c6bb3a0922d0f8d0cb29d023ff6380a653c23add
Fernando Pasik <fernando@pasik.com.ar>
2018-11-22 18:23:56 -0200

    test: rename and reorder reducer tests

--- src/__tests__/reducer.spec.ts
+++ src/__tests__/reducer.spec.ts
@@ -14,62 +14,66 @@ describe('Router Reducer', () => {
     expect(reducer()).toHaveProperty('routes', {});
   });
 
-  test('can set another active route', () => {
-    const path = '/contact';
-    const action = { type: 'SET_ACTIVE_ROUTE', path };
-    const newState = reducer(undefined, action);
-    expect(newState).toHaveProperty('activeRoute', path);
+  describe('add route', () => {
+    test('can add a path', () => {
+      const path = '/contact';
+      const action = { type: 'ADD_ROUTE', path };
+      const newState = reducer(undefined, action);
+      expect(newState.routes).toHaveProperty(path);
+    });
+
+    test('when adding a path the route can be active', () => {
+      const path = '/contact';
+      const action = { type: 'ADD_ROUTE', path };
+      const newState = reducer({ activeRoute: path, routes: {} }, action);
+      expect(newState.routes[path]).toHaveProperty('active', true);
+    });
   });
 
-  test('when setting active route checks for active path', () => {
-    const path = '/contact';
-    const routes = {
-      '/home': {},
-      '/contact': {},
-      '/about': {},
-    };
-    const action = { type: 'SET_ACTIVE_ROUTE', path };
-    const newState = reducer({ activeRoute: '/', routes }, action);
-    expect(newState.routes[path]).toHaveProperty('active', true);
-  });
+  describe('set active route', () => {
+    test('can set another route', () => {
+      const path = '/contact';
+      const action = { type: 'SET_ACTIVE_ROUTE', path };
+      const newState = reducer(undefined, action);
+      expect(newState).toHaveProperty('activeRoute', path);
+    });
 
-  test('when setting active route checks for active parameters', () => {
-    const activePath = '/products/shirt';
-    const path = '/products/:id';
-    const routes = {
-      '/products/:id': {},
-      '/about': {},
-    };
-    const action = { type: 'SET_ACTIVE_ROUTE', path: activePath };
-    const newState = reducer({ activeRoute: '/', routes }, action);
-    expect(newState.routes[path]).toHaveProperty('active', true);
-    expect(newState.routes[path]).toHaveProperty('params', { id: 'shirt' });
-  });
+    test('checks for active path', () => {
+      const path = '/contact';
+      const routes = {
+        '/home': {},
+        '/contact': {},
+        '/about': {},
+      };
+      const action = { type: 'SET_ACTIVE_ROUTE', path };
+      const newState = reducer({ activeRoute: '/', routes }, action);
+      expect(newState.routes[path]).toHaveProperty('active', true);
+    });
 
-  test('when setting active route parameters are empty', () => {
-    const activePath = '/about';
-    const path = '/products/:id';
-    const routes = {
-      '/products/:id': {},
-      '/about': {},
-    };
-    const action = { type: 'SET_ACTIVE_ROUTE', path: activePath };
-    const newState = reducer({ activeRoute: '/', routes }, action);
-    expect(newState.routes[path]).toHaveProperty('active', false);
-    expect(newState.routes[path]).toHaveProperty('params', { id: '' });
-  });
+    test('checks for active parameters', () => {
+      const activePath = '/products/shirt';
+      const path = '/products/:id';
+      const routes = {
+        '/products/:id': {},
+        '/about': {},
+      };
+      const action = { type: 'SET_ACTIVE_ROUTE', path: activePath };
+      const newState = reducer({ activeRoute: '/', routes }, action);
+      expect(newState.routes[path]).toHaveProperty('active', true);
+      expect(newState.routes[path]).toHaveProperty('params', { id: 'shirt' });
+    });
 
-  test('can add a path', () => {
-    const path = '/contact';
-    const action = { type: 'ADD_ROUTE', path };
-    const newState = reducer(undefined, action);
-    expect(newState.routes).toHaveProperty(path);
-  });
-
-  test('when adding a path the route can be active', () => {
-    const path = '/contact';
-    const action = { type: 'ADD_ROUTE', path };
-    const newState = reducer({ activeRoute: path, routes: {} }, action);
-    expect(newState.routes[path]).toHaveProperty('active', true);
+    test('parameters can be empty', () => {
+      const activePath = '/about';
+      const path = '/products/:id';
+      const routes = {
+        '/products/:id': {},
+        '/about': {},
+      };
+      const action = { type: 'SET_ACTIVE_ROUTE', path: activePath };
+      const newState = reducer({ activeRoute: '/', routes }, action);
+      expect(newState.routes[path]).toHaveProperty('active', false);
+      expect(newState.routes[path]).toHaveProperty('params', { id: '' });
+    });
   });
 });
