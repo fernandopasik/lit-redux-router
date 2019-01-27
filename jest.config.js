ad2168a77443ca5494b1b6c81e61fc42a966967c
Fernando Pasik <fernando@pasik.com.ar>
2019-01-27 00:29:04 +0000

    fix: remove jest config options that are now default

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,4 @@ module.exports = {
     '^.+\\.ts?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.(js|ts)?(x)', '**/?(*.)+(spec|test).(js|ts)?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
 };
