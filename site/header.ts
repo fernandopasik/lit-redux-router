52e335374b571b43f35ddbd85b1075f8d9fbf6b9
Fernando Pasik <fernando@pasik.com.ar>
2019-08-06 21:13:44 +0100

    refactor: use material web components

--- site/header.ts
+++ site/header.ts
@@ -1,45 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { LitElement, html, css, CSSResult, TemplateResult } from 'lit-element';
+import '@material/mwc-top-app-bar';
+import '@material/mwc-icon-button';
 
 class AppHeader extends LitElement {
   public static get styles(): CSSResult {
     return css`
-      .mdc-top-app-bar {
-        top: 0;
-      }
-
-      .mdc-top-app-bar__row {
-        padding: 0 8px;
-        max-width: 1260px;
-        margin: 0 auto;
-      }
-
-      .mdc-top-app-bar__title {
+      .title {
         color: inherit;
-        padding-left: 0;
+        text-decoration: none;
       }
     `;
   }
 
   public render(): TemplateResult {
     return html`
-      <style>
-        @import url(https://unpkg.com/@material/top-app-bar/dist/mdc.top-app-bar.min.css);
-      </style>
-      <header class="mdc-top-app-bar">
-        <div class="mdc-top-app-bar__row">
-          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-            <a href="/" class="mdc-top-app-bar__title">Lit Redux Router</a>
-          </section>
-          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
-            <a
-              href="https://github.com/fernandopasik/lit-redux-router"
-              class="mdc-top-app-bar__action-item"
-              aria-label="Github Repo"
-            >
-              <icon-github></icon-github>
-            </a>
-          </section>
-        </div>
+      <header>
+        <mwc-top-app-bar type="fixed">
+          <mwc-icon-button icon="menu" slot="navigationIcon"></mwc-icon-button>
+          <a slot="title" class="title" href="/">Lit Redux Router</a>
+          <a
+            href="https://github.com/fernandopasik/lit-redux-router"
+            aria-label="Github Repo"
+            slot="actionItems"
+          >
+            <icon-github color="white"></icon-github>
+          </a>
+        </mwc-top-app-bar>
       </header>
     `;
   }
