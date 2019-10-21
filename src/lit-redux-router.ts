c8df33a01a0da184d310403283b67cec5280c75e
Fernando Pasik <fernando@pasik.com.ar>
2019-10-21 21:47:34 +0100

    fix: remove unneeded eslint disable

--- src/lit-redux-router.ts
+++ src/lit-redux-router.ts
@@ -4,7 +4,6 @@ import { LazyStore } from 'pwa-helpers/lazy-reducer-enhancer';
 import Route from './route';
 import reducer from './reducer';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const connectRouter = (store: Store & LazyStore): void => {
   store.addReducers({ router: reducer });
 
