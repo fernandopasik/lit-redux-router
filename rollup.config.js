f0a7a9b759ecb47a6207c65de7ae487e2a0fb5a7
Fernando Pasik <fernando@pasik.com.ar>
2019-10-21 21:44:37 +0100

    style: use prettier on config file

--- rollup.config.js
+++ rollup.config.js
@@ -9,13 +9,12 @@ export default {
   },
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   onwarn(warning) {
-    const ignored = (
-      warning.code === 'THIS_IS_UNDEFINED' || (
-        warning.code === 'UNRESOLVED_IMPORT' && (
-          warning.source === 'lit-html/directives/unsafe-html' || warning.source === 'lit-element' || warning.source === 'pwa-helpers'
-        )
-      )
-    );
+    const ignored =
+      warning.code === 'THIS_IS_UNDEFINED' ||
+      (warning.code === 'UNRESOLVED_IMPORT' &&
+        (warning.source === 'lit-html/directives/unsafe-html' ||
+          warning.source === 'lit-element' ||
+          warning.source === 'pwa-helpers'));
 
     if (!ignored) {
       console.error(`(!) ${warning.message}`); // eslint-disable-line no-console
