1b6f3705184283659ca88511ae01ea1e482a1ded
Fernando Pasik <fernando@pasik.com.ar>
2020-02-24 01:12:39 +0000

    fix: add missing babel preset env

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/typescript'],
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
