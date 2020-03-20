431c3609be19abbff9da2a2f7173e4b6f619f2e7
Fernando Pasik <fernando@pasik.com.ar>
2020-03-20 21:15:41 +0000

    style: sort config props

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
   ],
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
 };
