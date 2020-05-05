70b5b45b1b0a1b8d73c5eec38936522c55b14547
Fernando Pasik <fernando@pasik.com.ar>
2020-05-05 01:56:41 +0100

    test: update jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.ts'],
-  testEnvironment: 'node',
+  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
+  transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents)/).*/'],
 };
