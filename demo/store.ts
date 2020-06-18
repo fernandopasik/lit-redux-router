2adf4f3013e07bbd7b5d296cfd98b4b69356f592
Fernando Pasik <fernando@pasik.com.ar>
2020-06-18 20:07:21 +0100

    refactor: redux devtools types fix

--- demo/store.ts
+++ demo/store.ts
@@ -3,16 +3,16 @@ import { combineReducers, compose, createStore } from 'redux';
 
 declare global {
   interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
 
 // eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ as typeof compose) || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?? compose;
 
 const store = createStore(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
   (state: any): any => state,
   composeEnhancers(lazyReducerEnhancer(combineReducers)),
 );
