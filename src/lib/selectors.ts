abe1d9ef409e93f83f21d28878de485bfd7f9bf0
Fernando Pasik <fernando@pasik.com.ar>
2020-06-18 19:57:00 +0100

    refactor: use Readonly type for State

--- src/lib/selectors.ts
+++ src/lib/selectors.ts
@@ -4,17 +4,17 @@ export interface State {
   router: RouterState;
 }
 
-export const getRoute = ({ router: { routes } }: State, route?: string): Route =>
+export const getRoute = ({ router: { routes } }: Readonly<State>, route?: string): Route =>
   (routes && route && routes[route]) || {};
 
-export const noRouteActive = ({ router: { routes } }: State): boolean =>
+export const noRouteActive = ({ router: { routes } }: Readonly<State>): boolean =>
   Object.keys(routes).reduce(
     (noActive: boolean, route: string): boolean => noActive && !routes[route].active,
     true,
   );
 
-export const isRouteActive = (state: State, route?: string): boolean =>
+export const isRouteActive = (state: Readonly<State>, route?: string): boolean =>
   route ? !!getRoute(state, route).active : noRouteActive(state);
 
-export const getRouteParams = (state: State, route?: string): RouteParams =>
+export const getRouteParams = (state: Readonly<State>, route?: string): RouteParams =>
   getRoute(state, route).params ?? {};
