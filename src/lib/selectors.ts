a89d3454bcdc761fbf174841665f2363a9a80cb1
Fernando Pasik <fernando@pasik.com.ar>
2020-06-20 22:14:17 +0100

    refactor: use deep readonly for selectors state

--- src/lib/selectors.ts
+++ src/lib/selectors.ts
@@ -1,20 +1,22 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import type { ReadonlyDeep } from 'type-fest';
 import { Route, RouteParams, RouterState } from './reducer';
 
 export interface State {
   router: RouterState;
 }
 
-export const getRoute = ({ router: { routes } }: Readonly<State>, route?: string): Route =>
+export const getRoute = ({ router: { routes } }: ReadonlyDeep<State>, route?: string): Route =>
   (routes && route && routes[route]) || {};
 
-export const noRouteActive = ({ router: { routes } }: Readonly<State>): boolean =>
+export const noRouteActive = ({ router: { routes } }: ReadonlyDeep<State>): boolean =>
   Object.keys(routes).reduce(
     (noActive: boolean, route: string): boolean => noActive && !routes[route].active,
     true,
   );
 
-export const isRouteActive = (state: Readonly<State>, route?: string): boolean =>
+export const isRouteActive = (state: ReadonlyDeep<State>, route?: string): boolean =>
   route ? !!getRoute(state, route).active : noRouteActive(state);
 
-export const getRouteParams = (state: Readonly<State>, route?: string): RouteParams =>
+export const getRouteParams = (state: ReadonlyDeep<State>, route?: string): RouteParams =>
   getRoute(state, route).params ?? {};
