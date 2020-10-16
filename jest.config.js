815e369c531ee6dc2b9ff1dc5ec18463e5450a76
Fernando Pasik <fernando@pasik.com.ar>
2020-10-16 03:16:25 +0100

    test: update ts-jest tsconfig parameter name

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s'],
-  globals: { 'ts-jest': { tsConfig: 'tsconfig.all.json' } },
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
 };
