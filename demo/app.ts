fernandopasik/lit-redux-router

commit 41b69c718c7edf88ee5146c5b9a01fa0b6f5221e

refactor: handle undefined values

src/lib/route.ts
@@ -51,13 +51,19 @@ export default (store: Readonly<Store & LazyStore>): void => {
     public scrollDisable = false;
 
     private setResolving(): void {
-      if (this.component && !window.customElements.get(this.component)) {
+      if (
+        typeof this.component !== 'undefined' &&
+        typeof window.customElements.get(this.component) === 'undefined'
+      ) {
         this.isResolving = true;
       }
     }
 
     private unsetResolving(): void {
-      if (this.component && window.customElements.get(this.component)) {
+      if (
+        typeof this.component !== 'undefined' &&
+        typeof window.customElements.get(this.component) === 'undefined'
+      ) {
         this.isResolving = false;
       }
     }

commit 37cd531e27c4902402526af7a5c249667d81a29c

Fix broken lazy loading (#52)
* refactor: remove unneeded whitespace

* fix: broken lazy loading resolved check

Resolving needs to be considered done once
the element is defined, not if the element
is undefined. This is a typo introduced in
41b69c718.

Also adapt the demo to use real lazy loading again.
The usage of .route="${import(…)}" means the component
is loaded by the time the <lit-route> is rendered,
not when the route actually becomes active.

demo/app.ts
@@ -42,6 +42,10 @@ class MyApp extends LitElement {
     store.dispatch({ type: 'TEST_FALSE' });
   }
 
+  public async importDocs(): Promise<void> {
+    await import('./docs');
+  }
+
   public render(): TemplateResult {
     return html`
       <style>
@@ -121,11 +125,11 @@ class MyApp extends LitElement {
             <h1>About Me</h1>
           </lit-route>
         </lit-route>
-        <lit-route path="/contact" component="my-contact" scrollDisable> </lit-route>
+        <lit-route path="/contact" component="my-contact" scrollDisable></lit-route>
         <lit-route
           path="/docs"
           component="docs-page"
-          .resolve="${import('./docs')}"
+          .resolve="${this.importDocs}"
           .scrollOpt="${{ behavior: 'smooth', block: 'end', inline: 'nearest' }}"
           loading="my-loading"
         ></lit-route>

src/lib/route.ts
@@ -158,7 +158,7 @@ export default (store: Readonly<LazyStore & Store>): void => {
     private unsetResolving(): void {
       if (
         typeof this.component !== 'undefined' &&
-        typeof window.customElements.get(this.component) === 'undefined'
+        typeof window.customElements.get(this.component) !== 'undefined'
       ) {
         this.isResolving = false;
       }