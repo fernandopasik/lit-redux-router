f4e0047b9c05450da5b5b60ecd95efd86c3b1918
Fernando Pasik <fernando@pasik.com.ar>
2021-03-09 03:31:06 +0000

    style: run prettier on config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,5 @@ export default {
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
-  moduleNameMapper: {
-    '(.*)\\.js': '$1',
-  },
+  moduleNameMapper: { '(.*)\\.js': '$1' },
 };
