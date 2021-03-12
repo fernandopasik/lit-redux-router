4aef7648b3657d403b5eae749236fab69d681e34
Fernando Pasik <fernando@pasik.com.ar>
2021-03-12 21:33:42 +0000

    style: sort prop in config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   collectCoverageFrom: ['src/**/*.{j,t}s'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
+  moduleNameMapper: { '(.*)\\.js': '$1' },
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
   transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
-  moduleNameMapper: { '(.*)\\.js': '$1' },
 };
