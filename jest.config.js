66cfea1acb83608ef205663bb58652007e33e686
Fernando Pasik <fernando@pasik.com.ar>
2021-05-22 11:10:26 +0100

    test: update config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ export default {
   collectCoverageFrom: ['src/**/*.{j,t}s'],
   globals: { 'ts-jest': { tsconfig: 'tsconfig.all.json' } },
   moduleNameMapper: { '(.*)\\.js': '$1' },
+  testEnvironment: 'jsdom',
   transform: { '^.+\\.[j|t]s$': 'ts-jest' },
-  transformIgnorePatterns: ['/node_modules/(?!(lit-html|lit-element|webcomponents|@open-wc)/).*/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@lit|lit|lit-html|lit-element|webcomponents|@open-wc)/).*/',
+  ],
 };
