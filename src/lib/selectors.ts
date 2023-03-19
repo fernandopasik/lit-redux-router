17a32b6a89c3257aa97a0ba8372c9524e3efbf06
Fernando Pasik <fernando@pasik.com.ar>
2023-03-19 00:09:40 +0000

    refactor: remove ReadonlyDeep use

--- src/lib/selectors.ts
+++ src/lib/selectors.ts
@@ -1,26 +1,20 @@
-import type { ReadonlyDeep } from 'type-fest';
 import type { Route, RouterState } from './reducer.js';
 
 export interface State {
   router: RouterState;
 }
 
-export const getRoute = (
-  { router: { routes } }: ReadonlyDeep<State>,
-  route?: string,
-): Route | undefined =>
+export const getRoute = ({ router: { routes } }: State, route?: string): Route | undefined =>
   typeof route !== 'undefined' && route in routes ? routes[route] : undefined;
 
-export const noRouteActive = ({ router: { routes } }: ReadonlyDeep<State>): boolean =>
+export const noRouteActive = ({ router: { routes } }: State): boolean =>
   Object.keys(routes).reduce(
     (noActive: boolean, route: string): boolean => noActive && !routes[route].active,
     true,
   );
 
-export const isRouteActive = (state: ReadonlyDeep<State>, route?: string): boolean =>
+export const isRouteActive = (state: State, route?: string): boolean =>
   typeof route !== 'undefined' ? Boolean(getRoute(state, route)?.active) : noRouteActive(state);
 
-export const getRouteParams = (
-  state: ReadonlyDeep<State>,
-  route?: string,
-): NonNullable<Route['params']> => getRoute(state, route)?.params ?? {};
+export const getRouteParams = (state: State, route?: string): NonNullable<Route['params']> =>
+  getRoute(state, route)?.params ?? {};
