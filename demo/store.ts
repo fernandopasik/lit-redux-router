5c0b1870848348342b9e127e84f67add8aae9b51
Fernando Pasik <fernando@pasik.com.ar>
2024-09-11 00:07:52 +0100

    style: ignore rule in line for now

--- demo/store.ts
+++ demo/store.ts
@@ -10,6 +10,7 @@ declare global {
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?? compose;
 
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 const store = createStore(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
   (state: any): any => state,
