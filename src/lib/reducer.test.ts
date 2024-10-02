73911d96e2c94a8bdc8503046ebae0e3d8473293
Fernando Pasik <fernando@pasik.com.ar>
2024-10-02 22:37:43 +0100

    style: disable rules in files

--- src/lib/reducer.test.ts
+++ src/lib/reducer.test.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/naming-convention, no-undefined */
 import { describe, expect, it } from '@jest/globals';
 import deepFreeze from 'deep-freeze';
 import reducer from './reducer.js';
